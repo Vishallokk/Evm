e6a466fadfd2d1ad3ec1f38d9d18910abda7a7df
Vishallokk <kushwahavishal04854@gmail.com>
2024-05-25 09:36:24 +0530

    winner

---
+++ script.js
@@ -0,0 +1,49 @@
+ const a = document.getElementById('bjp2');
+ const b = document.getElementById('congress2');
+ const c = document.getElementById('cycle2');
+ const d = document.getElementById('bsp2');
+ const e = document.getElementById('fng')
+
+
+let totalcount = 0;
+a.addEventListener("click",function(){
+   a.style.backgroundColor = "red"
+   setTimeout(() => {
+    a.style.backgroundColor = "black"
+   }, 1000);
+});
+
+
+b.addEventListener("click",function(){
+    a.style.backgroundColor = "red"
+    setTimeout(() => {
+     a.style.backgroundColor = "black"
+    }, 1000);
+ });
+
+ 
+ c.addEventListener("click",function(){
+    a.style.backgroundColor = "red"
+    setTimeout(() => {
+     a.style.backgroundColor = "black"
+    }, 1000);
+ });
+
+ 
+ d.addEventListener("click",function(){
+    a.style.backgroundColor = "red"
+    setTimeout(() => {
+     a.style.backgroundColor = "black"
+    }, 1000);
+ });
+
+ 
+ 
+ 
+
+
+
+ 
+ 
+
+
